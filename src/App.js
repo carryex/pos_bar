b997b80368af09c3264b9f134c5d4d269803764e
carryex <pokinsokha@gmail.com>
2021-01-07 00:03:33 +0100

    Add router in app. Add logo in assets. Add CustomButton and FormInput components. Create Sign In page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-function App() {
+import SignIn from "./pages/SignIn/SignIn";
+
+import "./App.scss";
+const App = () => {
+  const currentUser = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignIn />)}
+        />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+import FormInput from "../../components/FormInput/FormInput";
+import CustomButton from "../../components/CustomButton/CustomButton";
+
+import { ReactComponent as Logo } from "../../assets/logo_coffeebot.svg";
+
+import "./SignIn.scss";
+
+const SignIn = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // const { email, password } = state;
+    // try {
+    //   await auth.signInWithEmailAndPassword(email, password);
+    //   setState({ email: "", password: "" });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  return (
+    <div className="sign-in">
+      <div className="container">
+        <div className="logo-container">
+          <Logo />
+        </div>
+        <form onSubmit={handleSubmit}>
+          {/* here goas FormInput and buttons*/}
+          <FormInput
+            type="email"
+            name="email"
+            label="email"
+            value={state.email}
+            handleChange={handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="password"
+            value={state.password}
+            handleChange={handleChange}
+            required
+          />
+          <div className="button">
+            <CustomButton type="submit">Sign in</CustomButton>
+          </div>
+          <div className="button">
+            <CustomButton type="submit">Registration</CustomButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default SignIn;
